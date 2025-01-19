4b774a88ad69ab38a0cfa189232c0752f9134290
rudemaru <ruslandemidovich2005@gmail.com>
2025-01-19 05:17:21 +0300

    postgres db connection w/ gorm

--- internal/bot/menu.go
+++ internal/bot/menu.go
@@ -4,8 +4,27 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const (
+	AddSetButtonText                   string = "Добавить подход"
+	NextExerciseButtonText             string = "Следующее упражнение"
+	NewSessionButtonText               string = "Новая тренировка"
+	PreviousSessionsButtonText         string = "Прошлые тренировки"
+	StatisticsButtonText               string = "Статистика"
+	IndividualDataButtonText           string = "Индивидуальные данные"
+	EditSessionButtonText              string = "Редактировать тренировку"
+	EditPreviousSessionButtonText      string = "Редактировать завершенную тренировку"
+	MainMenuButtonText                 string = "Главное меню"
+	AllTimeStatisticsButtonText        string = "Статистика за все время"
+	OverTimePeriodStatisticsButtonText string = "Статистика за период"
+	ShowLastSessionsButtonText         string = "Показать последние 10 тренировок"
+)
+
+var (
+	TimerButtonText string = "Начать тренировку"
+	CurrentPage     string = "Main menu" // Main menu - Current session - Previous sessions - Statistics
+)
+
 func createMenu() tgbotapi.ReplyKeyboardMarkup {
-	var TimerButtonText, AddSetButtonText, SkipSetButton string
 
 	if timerRunning {
 		TimerButtonText = "Завершить тренировку"
@@ -13,17 +32,44 @@ func createMenu() tgbotapi.ReplyKeyboardMarkup {
 		TimerButtonText = "Начать тренировку"
 	}
 
-	AddSetButtonText = "Занести подход"
-	SkipSetButton = "Пропустить подход"
+	var keyboard tgbotapi.ReplyKeyboardMarkup
 
-	keyboard := tgbotapi.NewReplyKeyboard(
-		tgbotapi.NewKeyboardButtonRow(
-			tgbotapi.NewKeyboardButton(AddSetButtonText), tgbotapi.NewKeyboardButton(SkipSetButton),
-		),
-		tgbotapi.NewKeyboardButtonRow(
-			tgbotapi.NewKeyboardButton(TimerButtonText),
-		),
-	)
+	switch CurrentPage {
+	case "Main menu":
+		{
+			keyboard = tgbotapi.NewReplyKeyboard(
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(NewSessionButtonText), tgbotapi.NewKeyboardButton(PreviousSessionsButtonText)),
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(StatisticsButtonText)))
+		}
+	case "Current session":
+		{
+			keyboard = tgbotapi.NewReplyKeyboard(
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(AddSetButtonText), tgbotapi.NewKeyboardButton(NextExerciseButtonText)),
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(IndividualDataButtonText), tgbotapi.NewKeyboardButton(EditSessionButtonText)),
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(MainMenuButtonText), tgbotapi.NewKeyboardButton(TimerButtonText)),
+			)
+		}
+	case "Previous sessions":
+		{
+			keyboard = tgbotapi.NewReplyKeyboard(
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(ShowLastSessionsButtonText), tgbotapi.NewKeyboardButton(EditPreviousSessionButtonText)),
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(MainMenuButtonText)),
+			)
+		}
+	case "Statistics":
+		{
+			keyboard = tgbotapi.NewReplyKeyboard(
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(AllTimeStatisticsButtonText), tgbotapi.NewKeyboardButton(OverTimePeriodStatisticsButtonText)),
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(MainMenuButtonText)),
+			)
+		}
+	default:
+		{
+			keyboard = tgbotapi.NewReplyKeyboard(
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(NewSessionButtonText), tgbotapi.NewKeyboardButton(PreviousSessionsButtonText)),
+				tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton(StatisticsButtonText)))
+		}
+	}
 
 	return keyboard
 }
---
+++ internal/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	config "github.com/rudemaru/TG-GYM-BOT/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB(cfg *config.Config) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+		cfg.Postgres.Host,
+		cfg.Postgres.User,
+		cfg.Postgres.Password,
+		cfg.Postgres.DBName,
+		cfg.Postgres.Port,
+		cfg.Postgres.SSLMode,
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+}
